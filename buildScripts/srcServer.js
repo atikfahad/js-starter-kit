94c89a4109cc458f4b2998681b9b96976afba2a8
atikfahad <fahadatik@gmail.com>
2019-06-19 16:19:47 +0600

    Set up lint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.dev';
+
+/* eslint-disable no-console */ // This will override eslint rules
 
 const port = 3000;
 const app = express();
+const compiler = webpack(webpackConfig);
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: webpackConfig.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
