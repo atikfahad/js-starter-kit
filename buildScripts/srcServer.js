83f031d0a929b12a3527e5d46f76b521c9f41ec1
atikfahad <fahadatik@gmail.com>
2019-06-19 12:24:04 +0600

    Initialize express server and configure start script

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+})
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(`Express Server running at:${port}`)
+    open(`http://localhost:${port}`);
+  }
+})
